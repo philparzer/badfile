26b070701a1015d67a545d36241b861f126158b2
Philipp Parzer <phil0898.pp@gmail.com>
2023-05-19 23:07:29 +0200

    uncomment block in middlewware

--- middleware.ts
+++ middleware.ts
@@ -21,16 +21,16 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
+  // If you have one
+  if (
+    [
+      '/manifest.json',
+      '/favicon.ico',
+      '/images'
+    ].includes(pathname)
+  )
+    return
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
@@ -48,10 +48,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-}
+  // do not localize next.js paths
+    matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)",],
+  };
