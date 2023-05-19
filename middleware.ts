ed6e944907918e75f028bd3c4950267ae9e122d7
Philipp Parzer <phil0898.pp@gmail.com>
2023-05-19 23:12:09 +0200

    add exclusion to matcher

--- middleware.ts
+++ middleware.ts
@@ -21,17 +21,6 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // If you have one
-  if (
-    [
-      '/manifest.json',
-      '/favicon.ico',
-      '/images'
-    ].includes(pathname)
-  )
-    return
-
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
@@ -49,5 +38,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // do not localize next.js paths
-    matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)",],
+    matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|images/.*).*)",],
   };
