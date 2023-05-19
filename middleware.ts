5d24f3952dd87e275cdd0007b4c58636018af48c
Philipp Parzer <phil0898.pp@gmail.com>
2023-05-19 23:03:54 +0200

    change matcher

--- middleware.ts
+++ middleware.ts
@@ -48,6 +48,10 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // do not localize next.js paths
-    matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)",],
-  };
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+}
